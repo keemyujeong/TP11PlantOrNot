0175073917dd26a828c94407d43901615197e72f
keemyujeong <thinkrightkim@gmail.com>
2022-11-02 15:15:00 +0900

    First

---
+++ app/src/main/java/com/kyjsoft/tp11plantornot/HomeAdapter.kt
@@ -0,0 +1,22 @@
+package com.kyjsoft.tp11plantornot
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.VH>() {
+
+    inner class VH(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/kyjsoft/tp11plantornot/HomeRecyclerItem.kt
+++ app/src/main/java/com/kyjsoft/tp11plantornot/HomeRecyclerItem.kt
@@ -1,3 +1,3 @@
 package com.kyjsoft.tp11plantornot
 
-class HomeRecyclerItem(var title: String, var text: String)
+data class HomeRecyclerItem(var title: String, var text: String)
