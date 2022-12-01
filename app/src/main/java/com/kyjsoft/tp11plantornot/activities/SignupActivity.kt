15826633bb062270fa06a383d67825855f142028
keemyujeong <thinkrightkim@gmail.com>
2022-12-01 17:45:15 +0900

    dev 12/1

--- app/src/main/java/com/kyjsoft/tp11plantornot/activities/SignupActivity.kt
+++ app/src/main/java/com/kyjsoft/tp11plantornot/activities/SignupActivity.kt
@@ -2,13 +2,37 @@ package com.kyjsoft.tp11plantornot.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import com.kyjsoft.tp11plantornot.R
+import com.kyjsoft.tp11plantornot.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
 
-
+    val binding by lazy { ActivitySignupBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+
+        binding.btnSignup.setOnClickListener { clickSignUp() }
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    private fun clickSignUp(){
+
+
 
     }
 }
